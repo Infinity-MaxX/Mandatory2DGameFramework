c21574febf254c89ddc07d591b7cafd990f72ac1
Infinity-MaxX <katerina@oestergaard.com>
2026-04-15 14:40:47 +0200

    Add project files.

---
+++ Mandatory2DGameFramework/model/Cretures/Creature.cs
@@ -0,0 +1,56 @@
+﻿using Mandatory2DGameFramework.model.attack;
+using Mandatory2DGameFramework.model.defence;
+using Mandatory2DGameFramework.worlds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandatory2DGameFramework.model.Cretures
+{
+    // This needs to be a Template Design Pattern
+    // It needs to support the Observer Design Pattern
+    // At least one method needs to implement the Strategy Design Pattern
+    public class Creature
+    {
+        public string Name { get; set; }
+        public int HitPoint { get; set; }
+        // Todo consider how many attack / defence weapons are allowed
+        public AttackItem? Attack { get; set; }
+        public DefenceItem? Defence { get; set; }
+
+        public Creature()
+        {
+            Name = string.Empty;
+            HitPoint = 100;
+
+            // if multiple attack items are equiped then the creature
+            // should hit opponent with the sum of all equipped weapons
+            Attack = null;
+            // when a creature is hit, the defence items will reduce
+            // the incoming attack damage by the sum of ReduceHitPoint
+            Defence = null;
+        }
+
+        public int Hit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReceiveHit(int hit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Loot(WorldObject obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Name)}={Name}, {nameof(HitPoint)}={HitPoint.ToString()}, {nameof(Attack)}={Attack}, {nameof(Defence)}={Defence}}}";
+        }
+    }
+}
---
+++ Mandatory2DGameFramework/model/attack/AttackItem.cs
@@ -0,0 +1,37 @@
+﻿using Mandatory2DGameFramework.worlds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandatory2DGameFramework.model.attack
+{
+    // There needs to be a subclass for AttackItem that uses the
+    // Decorator Design Pattern: example: boost or weaken defence
+    // There needs to be a subclass for AttackItem that uses the
+    // Composite Design Pattern; it needs to store weapons
+
+    // Note: The AttackItem must have a property weight. A creature
+    // must be configurable to be able to carry a maximum weight of
+    // attack weapons (attackItems). Hint: May Implemented in the
+    // Composite class
+    public class AttackItem : WorldObject
+    {
+        public string Name { get; set; }
+        public int Hit { get; set; }
+        public int Range { get; set; }
+
+        public AttackItem()
+        {
+            Name = string.Empty;
+            Hit = 0;
+            Range = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Name)}={Name}, {nameof(Hit)}={Hit.ToString()}, {nameof(Range)}={Range.ToString()}}}";
+        }
+    }
+}
