8d7be9ccf5dd47ff210cfd4c4efffe6ed7c47967
Infinity-MaxX <katerina@oestergaard.com>
2026-04-15 20:39:50 +0200

    Added XML documentation to AttackItem.cs

--- Mandatory2DGameFramework/model/attack/AttackItem.cs
+++ Mandatory2DGameFramework/model/attack/AttackItem.cs
@@ -16,24 +16,67 @@ namespace Mandatory2DGameFramework.model.attack
     // must be configurable to be able to carry a maximum weight of
     // attack weapons (attackItems). Hint: May Implemented in the
     // Composite class
+
+    /// <summary>
+    /// Represents an item that can be used to perform attacks within 
+    /// the game world. Provides properties for the item's name, attack
+    /// strength, and range.
+    /// </summary>
+    /// <remarks>AttackItem serves as a base class for specialized attack 
+    /// items, including those that implement design patterns such as 
+    /// Decorator (for modifying attack or defense properties) and Composite 
+    /// (for grouping multiple weapons). Subclasses may extend functionality 
+    /// to support features like weight management or composite behaviors. 
+    /// Instances of AttackItem are typically used by creatures or entities 
+    /// capable of carrying and utilizing attack items.</remarks>
     public class AttackItem : WorldObject
     {
+        #region Properties
+        /// <summary>
+        /// Gets or sets the name of the attack item, which 
+        /// identifies the type of attack or weapon it represents.
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the hit strength of the attack item, 
+        /// which determines the amount of damage it can inflict 
+        /// on a target when used in an attack.
+        /// </summary>
         public int Hit { get; set; }
+        /// <summary>
+        /// Gets or sets the range of the attack item, which 
+        /// indicates how far the attack can reach or affect 
+        /// targets. This could represent the distance at which 
+        /// the attack can be effective, such as melee range or 
+        /// ranged attacks.
+        /// </summary>
         public int Range { get; set; }
+        #endregion
 
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the AttackItem class with default values.
+        /// </summary>
         public AttackItem()
         {
             Name = string.Empty;
             Hit = 0;
             Range = 0;
         }
+        #endregion
 
+        #region Methods
+        /// <summary>
+        /// A string representation of the AttackItem, 
+        /// including its name, hit strength, and range.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return $"{{{nameof(Name)}={Name}, " +
                 $"{nameof(Hit)}={Hit.ToString()}, " +
                 $"{nameof(Range)}={Range.ToString()}}}";
         }
+        #endregion
     }
 }
