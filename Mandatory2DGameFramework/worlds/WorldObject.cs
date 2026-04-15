d5c908e9f155c37a35061f0f9f4dfb627ad1125b
Infinity-MaxX <katerina@oestergaard.com>
2026-04-15 20:32:11 +0200

    Added XML documentation to WorldObject.cs

--- Mandatory2DGameFramework/worlds/WorldObject.cs
+++ Mandatory2DGameFramework/worlds/WorldObject.cs
@@ -6,24 +6,51 @@ using System.Threading.Tasks;
 
 namespace Mandatory2DGameFramework.worlds
 {
+    /// <summary>
+    /// Represents an object within the world with a name 
+    /// and properties indicating whether it can be looted
+    /// or removed.
+    /// </summary>
     public class WorldObject
     {
+        #region Properties
+        /// <summary>
+        /// Gets or sets the name associated with the object.
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating whether the item can be looted.
+        /// </summary>
         public bool Lootable { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating whether the item can be removed.
+        /// </summary>
         public bool Removeable { get; set; }
+        #endregion
 
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the WorldObject class with default values.
+        /// </summary>
         public WorldObject()
         {
             Name = string.Empty;
             Lootable = false;
             Removeable = false;
         }
+        #endregion
 
+        #region Methods
+        /// <summary>
+        /// A string representation of the WorldObject, including its name and properties.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return $"{{{nameof(Name)}={Name}, " +
                 $"{nameof(Lootable)}={Lootable.ToString()}, " +
                 $"{nameof(Removeable)}={Removeable.ToString()}}}";
         }
+        #endregion
     }
 }
