97f47b62cc20306dfab2ddbcace90d2d22d3b5ce
Infinity-MaxX <katerina@oestergaard.com>
2026-04-15 20:34:53 +0200

    Added XML comments to DefenceItem.cs

--- Mandatory2DGameFramework/model/defence/DefenceItem.cs
+++ Mandatory2DGameFramework/model/defence/DefenceItem.cs
@@ -7,20 +7,42 @@ using System.Threading.Tasks;
 
 namespace Mandatory2DGameFramework.model.defence
 {
+    /// <summary>
+    /// Represents an object in the world that provides 
+    /// defensive capabilities by reducing incoming hit points.
+    /// </summary>
     public class DefenceItem : WorldObject
     {
+        #region Properties
+        /// <summary>
+        /// Gets or sets the amount of hit points to reduce 
+        /// from the creature when damage is applied.
+        /// </summary>
         public int ReduceHitPoint { get; set; }
+        #endregion
 
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the DefenceItem class with default values.
+        /// </summary>
         public DefenceItem()
         {
             Name = string.Empty;
             ReduceHitPoint = 0;
         }
+        #endregion
 
+        #region Methods
+        /// <summary>
+        /// A string representation of the DefenceItem, including
+        /// its name and the amount of hit points it reduces.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return $"{{{nameof(Name)}={Name}, " +
                 $"{nameof(ReduceHitPoint)}={ReduceHitPoint.ToString()}}}";
         }
+        #endregion
     }
 }
