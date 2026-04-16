235c03b8a06ee68649bafc27b52122bffe1f6636
Infinity-MaxX <katerina@oestergaard.com>
2026-04-16 16:24:13 +0200

    Implemented a random number generator for attack damage and damage taken. Attempted Doxygen, will remain in uncommitted changes.

---
+++ Mandatory2DGameFramework/NumberGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandatory2DGameFramework.generator
+{
+    /// <summary>
+    /// A utility class for generating random numbers within 
+    /// a specified range. This will be used to generate the
+    /// amount of damage a creature can do and the amount of
+    /// hit points a creature will lose when hit.
+    /// </summary>
+    public class NumberGenerator
+    {
+        #region Instances
+        private Random _generator;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the NumberGenerator class,
+        /// with default settings for the random generator.
+        /// </summary>
+        public NumberGenerator()
+        {
+            _generator = new Random();
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Returns a random number in the interval between the values
+        /// of "min" and "max"
+        /// </summary>
+        public int Next(int min, int max)
+        {
+            int value = min + _generator.Next(max - min + 1);
+            return value;
+        }
+        #endregion
+    }
+}
