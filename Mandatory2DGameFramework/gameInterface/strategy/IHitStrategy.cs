5c551421d6117c4a3890743fa17a26d4f0c159ab
Infinity-MaxX <katerina@oestergaard.com>
2026-04-17 20:26:36 +0200

    Added IHitStrategy.cs interface

---
+++ Mandatory2DGameFramework/gameInterface/strategy/IHitStrategy.cs
@@ -0,0 +1,28 @@
+﻿using Mandatory2DGameFramework.model.attack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandatory2DGameFramework.gameInterface.strategy
+{
+    /// <summary>
+    /// Interface for calculating the damage of an attack 
+    /// based on a collection of attack items. Implementations 
+    /// of this interface can define different strategies for 
+    /// how the damage is calculated.
+    /// </summary>
+    public interface IHitStrategy
+    {
+        /// <summary>
+        /// Calculates the total damage of an attack based on 
+        /// the provided collection of attack items.
+        /// </summary>
+        /// <param name="items">The collection of attack items 
+        /// to calculate damage from.</param>
+        /// <returns>The total damage calculated from the attack 
+        /// items.</returns>
+        int CalculateDamage(IEnumerable<AttackItem> items);
+    }
+}
