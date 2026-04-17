7727b5ab26bda5bf0af04c6b1ced09607a781ec5
Infinity-MaxX <katerina@oestergaard.com>
2026-04-17 16:11:20 +0200

    Added an AttackDebuffDecorator.cs that depowers the hit strength of an attack item.

---
+++ Mandatory2DGameFramework/model/attack/decorators/AttackDebuffDecorator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mandatory2DGameFramework.model.attack;
+
+namespace Mandatory2DGameFramework.model.attack.decorators
+{
+    /// <summary>
+    /// Reduces the hit strength of the wrapped attack item.
+    /// </summary>
+    public class AttackDebuffDecorator : AttackDecorator
+    {
+        #region Instances
+        private readonly int _debuff;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets the attack subtracted by the debuff. A check
+        /// is employed to ensure the damage output does not return a
+        /// a negative value. Subclasses may override this to modify 
+        /// the debuff value.
+        /// </summary>
+        public override int Hit
+        {
+            get
+            {
+                int debuff = base.Hit - _debuff;
+                return debuff < 0 ? 0 : debuff;
+            }
+            set { base.Hit = value; }
+        }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new decorator that wraps the specified attack
+        /// item with added debuff.
+        /// </summary>
+        /// <param name="item">The attack item to debuff.</param>
+        /// <param name="debuff">The amount to debuff the attack item
+        /// with. Default value is 3.</param>
+        public AttackDebuffDecorator(AttackItem item, int debuff = 3)
+            : base(item)
+        {
+            _debuff = debuff;
+            Name = item.Name + " -AttackDebuff";
+        }
+        #endregion
+    }
+}
+
