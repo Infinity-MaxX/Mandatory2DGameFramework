0a75c02f827285711a0062e106dac3a9584ed0e5
Infinity-MaxX <katerina@oestergaard.com>
2026-04-17 20:23:21 +0200

    Added ICreatureObserver.cs interface with XML comments.

---
+++ Mandatory2DGameFramework/gameInterface/observer/ICreatureObserver.cs
@@ -0,0 +1,30 @@
+﻿using Mandatory2DGameFramework.model.Cretures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandatory2DGameFramework.gameInterface.observer
+{
+    /// <summary>
+    /// Interface for observing events related to creatures, 
+    /// such as being hit or dying.
+    /// </summary>
+    public interface ICreatureObserver
+    {
+        /// <summary>
+        /// A method that is called when a creature is hit, 
+        /// providing the creature and the damage taken.
+        /// </summary>
+        /// <param name="creature">The creature that was hit.</param>
+        /// <param name="damage">The amount of damage taken by the 
+        /// creature.</param>
+        void OnCreatureHit(Creature creature, int damage);
+        /// <summary>
+        /// A method that is called when a creature dies.
+        /// </summary>
+        /// <param name="creature">The creature that died.</param>
+        void OnCreatureDeath(Creature creature);
+    }
+}
