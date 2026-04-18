5829322ea087f2e9b5be0f5acbda9136b970760b
Infinity-MaxX <katerina@oestergaard.com>
2026-04-18 16:06:20 +0200

    Added defence buff and debuff decorators.

---
+++ Mandatory2DGameFramework/model/defence/decorators/DefenceBuffDecorator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mandatory2DGameFramework.model.defence;
+
+namespace Mandatory2DGameFramework.model.defence.decorators
+{
+    /// <summary>
+    /// Increases the reduce hit point of the wrapped defence item.
+    /// </summary>
+    public class DefenceBuffDecorator : DefenceDecorator
+    {
+        #region Instances
+        private readonly int _buff;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets the defence item with added buff.
+        /// Subclasses may override this to modify the buff value.
+        /// </summary>
+        public override int ReduceHitPoint
+        {
+            get { return base.ReduceHitPoint + _buff; }
+            set { base.ReduceHitPoint = value; }
+        }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new decorator that wraps the specified defence
+        /// item with added buff.
+        /// </summary>
+        /// <param name="baseDefence">The defence item to buff.</param>
+        /// <param name="buff">The amount to buff the defence item with.
+        /// Default value is 5.</param>
+        public DefenceBuffDecorator(DefenceItem baseDefence, int buff = 5)
+            : base(baseDefence)
+        {
+            _buff = buff;
+            Name = baseDefence.Name + $" +{buff}";
+        }
+        #endregion
+    }
+}
---
+++ Mandatory2DGameFramework/model/defence/decorators/DefenceDebuffDecorator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mandatory2DGameFramework.model.defence;
+
+namespace Mandatory2DGameFramework.model.defence.decorators
+{
+    /// <summary>
+    /// Reduces the reduce hit point of the wrapped defence item.
+    /// </summary>
+    public class DefenceDebuffDecorator : DefenceDecorator
+    {
+        #region Instances
+        private readonly int _debuff;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets the defence item with added debuff. A check
+        /// is employed to ensure the damage output does not return a
+        /// negative value. Subclasses may override this to modify the
+        /// debuff value.
+        /// </summary>
+        public override int ReduceHitPoint
+        {
+            get
+            {
+                int debuff = base.ReduceHitPoint - _debuff;
+                if (debuff < 0) { return 0; } else { return debuff; }
+            }
+            set { base.ReduceHitPoint = value; }
+        }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new decorator that wraps the specified defence
+        /// item with added debuff.
+        /// </summary>
+        /// <param name="baseDefence">The defence item to debuff.</param>
+        /// <param name="debuff">The amount to debuff the defence item
+        /// with. Default value is 3.</param>
+        public DefenceDebuffDecorator(DefenceItem baseDefence, int debuff = 3)
+            : base(baseDefence)
+        {
+            _debuff = debuff;
+            Name = baseDefence.Name + $" -{debuff}";
+        }
+        #endregion
+    }
+}
+
